26651deaaf701bc6d0eae20afb00b5435a06f0e5
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-04-28 12:42:14 +0300

    add configs,BeforeEach and AfterEach

---
+++ src/test/java/docuSketch/ConfProperties.java
@@ -0,0 +1,11 @@
+package docuSketch;
+
+public class ConfProperties {
+
+
+        protected static final String GMAILUSERNAME = "qae4488@gmail.com";
+
+        protected static final String GMAILPASSWORD = "44884488";
+    }
+
+
