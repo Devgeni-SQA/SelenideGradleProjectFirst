edf20d2b2d7d7e07e682badf462baaf8549ef4b6
Evgeniia Demchenko <demchenkoeb04022012@gmail.com>
2023-05-04 08:53:28 +0300

    add step go to the next page to testFirst for project (#9)

--- src/main/java/immoviewer/OrdersTestLocators.java
+++ src/main/java/immoviewer/OrdersTestLocators.java
@@ -12,4 +12,6 @@ public class OrdersTestLocators {
             "//div[@id='mat-select-value-5']/span");
     public static SelenideElement buttonOfStatusDone = $x("//div[@id='mat-select-4-panel']//p[contains(text(),'DONE')]");
     public static ElementsCollection statusOfSelectedRow = $$x("//tr[@role='row']//p[@class='mat-caption m-0 bold status-font']");
+    public static SelenideElement buttonNextPage = $x("//pagination-template//a[@aria-label=' page']");
+    public static SelenideElement numberOfPages = $x("//pagination-template//a[@tabindex='0']/span[2]");
 }
