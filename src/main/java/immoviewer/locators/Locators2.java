50aa34a8a6e6baef5a41e7dc0f2964125796ac64
Evgeniia Demchenko <demchenkoeb04022012@gmail.com>
2023-05-17 14:36:34 +0300

    Rf|test2 (#17)
    * RF new test
    
    * RF new test

--- src/main/java/immoviewer/locators/Locators2.java
+++ src/main/java/immoviewer/locators/Locators2.java
@@ -7,15 +7,16 @@ import static com.codeborne.selenide.Selenide.*;
 public class Locators2 {
 
     public static SelenideElement tabAssignee = $x("//div[@class='orders-filter']//div[4]//div/div/span[not(contains(@class, 'ripple'))]");
-    public static SelenideElement tabStatus = $x("//div[@id='mat-select-value-5']/span");
+    public static SelenideElement tabStatus = $x("//*[@class='filter-status']//div[contains(@id, 'select')]/span");
     public static SelenideElement tabCustomer = $x("//body[@class='ff-app']//div[@class='filter-customer']//span");
     public static SelenideElement sendExternalProviderButton = $x("//span[text()=' Send to External Provider ']/ancestor::button");
     public static SelenideElement inputTime = $x("//input[@type='number']");
     public static SelenideElement logTimeAndFinishButton = $x("//span[text()='Log Time & Finish']");
     public static SelenideElement leftsideMenuOrdersButton = $x("//button[@routerlink='/orders']/span[@class='mat-button-wrapper']");
-    public static SelenideElement IdOfFirstOrder = $("td:first-child p.mat-display-1");////div[@class='table-td']//p
+    public static SelenideElement IdOfFirstOrder = $("td:first-child p.mat-display-1");
     public static SelenideElement clearFilterOnAssigneeButton = $x("//button[@class='clear-button']/i");
     public static ElementsCollection typesOfcustomers = $$x("//mat-option//p");
+    public static ElementsCollection firstTabOfOrderRow = $$("tbody>tr");
     public static SelenideElement newIterationButton = $x("//div[@class='order-control-button']/button");
     public static SelenideElement startDrawingButton = $x("//span[text()=' Start drawing ']/ancestor::button");
 }
